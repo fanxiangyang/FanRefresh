dbcfe38fa32ca5fbde531cdf4ba084c6aaddfdcb
fanxiangyang <fanxiangyang_heda@163.com>
2017-03-29 11:29:57 +0800

    Initial Commit

---
+++ FanRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FanRefresh
+//
+//  Created by 向阳凡 on 2017/3/29.
+//  Copyright © 2017年 凡向阳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
